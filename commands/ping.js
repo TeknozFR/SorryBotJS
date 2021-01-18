7473e95b5cdd5101cf2ad9ee4eea3a9195373177
TeknozFR <teknoz.fr@gmail.com>
2021-01-18 09:07:13 -0500

    update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,23 +3,7 @@ module.exports = {
     description: 'Ping command',
     async execute(message){
 
-        const fs = require ("fs");
-
         message.channel.send("Pong! The bot is online.");
-
-        var youtubeAPIObject = JSON.parse(fs.readFileSync("./json_files/youtube_api.json"));
-
-        for (previousID of youtubeAPIObject.previous_videos){
-            console.log(previousID);
-        }
-
-        var latestVideo = youtubeAPIObject.youtube[0].latestVideoID;
-
-        if (!youtubeAPIObject.previous_videos.includes(latestVideo)){
-            console.log("its not in there");
-            
-        }
-
 
     }
 }
